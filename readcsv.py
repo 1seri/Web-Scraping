2e6ef51b95159893c4d6c75e8e2e7fba74881b8b
1seri <47559527+1seri@users.noreply.github.com>
2020-01-16 22:15:51 +0100

    Create readcsv.py

---
+++ readcsv.py
@@ -0,0 +1,29 @@
+import csv
+import smtplib
+with open('skedar.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter = ',')
+    try:
+        for row in readCSV:
+            if row[0] =='+355 (0) 69 3858 950' and row[1] == 'info [at] w3development [dot] net':
+                    print('Numri i kontaktit dhe emaili, nuk kane ndryshuar!')
+            else:
+                print('Te dhenat kane ndryshuar....nje email eshte derguar')
+
+    except IndexError:
+            print(" ")
+
+sender = "Private Person <from@smtp.mailtrap.io>"
+receiver = "A Test User <to@smtp.mailtrap.io>"
+
+message = f"""\
+Subject: Hi Mailtrap
+To: {receiver}
+From: {sender}
+
+Seri this works thanks God."""
+
+with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+    server.login("984f98daf98eb1", "5aee14525c9521")
+    server.sendmail(sender, receiver, message)
+
+
